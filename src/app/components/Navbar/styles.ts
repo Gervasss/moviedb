fc5284d05e1488a37c40944b5b4bb4f3d368dd94
Gervasss <gervasiobrito10@gmail.com>
2025-08-05 14:00:32 -0300

    fix mobile admin-home

--- src/app/components/Navbar/styles.ts
+++ src/app/components/Navbar/styles.ts
@@ -9,6 +9,11 @@ export const Navbar = styled.div<NavbarProps>`
 .Navbar{
  background-color: ${({ darkMode }) => (darkMode ? '#333' : '#fff')}; 
 }
+.mobile-menu-list{
+ background-color: ${({ darkMode }) => (darkMode ? '#333' : '#fff')}; 
+ color: ${({ darkMode }) => (darkMode ? '#fff' : '#333')}; 
+ 
 
+}
 `;
 
